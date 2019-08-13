ef3f47449fce4c99eabd30b54065c208daf990b8
3vilguy <2122619+3vilguy@users.noreply.github.com>
2019-08-14 00:57:38 +0100

    Trying Trumbowyg and ckeditor inside webview

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { WebView } from 'react-native-webview';
 const App = () => {
   return (
     <WebView
-      source={{ uri: 'http://google.com/' }}
+      source={{ uri: 'http://3vil.eu/Notesmaster/trumbowyg-jquery/' }}
+      // source={{ uri: 'http://3vil.eu/Notesmaster/ckeditor-4-full-all/index.html' }}
       style={{ marginTop: 20 }}
     />
   );
